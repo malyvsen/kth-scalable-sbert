fd93e7004b0ff610600ec98da861e5c2e48322e3
malyvsen <5940672+malyvsen@users.noreply.github.com>
2021-12-05 23:13:12 +0100

    chore: rename vectorized file

--- sbert/data.py
+++ sbert/data.py
@@ -3,4 +3,4 @@ from pathlib import Path
 
 data_path = Path(__file__).parent.parent / "data"
 searchable_path = data_path / "news.json"
-searchable_vectors_path = data_path / "news_searched.pkl"
+searchable_vectors_path = data_path / "news_vectors.pkl"
